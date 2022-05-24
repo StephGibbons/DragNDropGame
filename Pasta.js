a04870d35236d84dbf31ff3430b577087049cbe8
StephGibbons <94647859+StephGibbons@users.noreply.github.com>
2022-05-24 14:50:45 -0230

    'master'

--- Pasta.js
+++ Pasta.js
@@ -1,21 +1,56 @@
-
-const circleorange = document.getElementById('circleorange');
-
-const orangeBlock = document.querySelector('.ColorOrange');
-
 var score = 0;
 
+const timer = document.getElementById("timer");
+let timerInterval;
 
-//Circle Listeners
+startTimer = () => {
+  // Firs twe start by clearing the existing timer, in case of a restart
+  clearInterval(timerInterval);
+  // Then we clear the variables
+  let second = 0,
+    minute = 0,
+    hour = 0;
+
+  // Next we set a interval every 1000 ms
+  timerInterval = setInterval(function () {
+    // Toggle the odd class every interval
+timer.classList.toggle('odd');
+
+    // We set the timer text to include a two digit representation
+    timer.innerHTML =
+      (hour ? hour + ":" : "") +
+      (minute < 10 ? "0" + minute : minute) +
+      ":" +
+      (second < 10 ? "0" + second : second);
+
+    // Next we add a new second since one second is passed
+    second++;
+
+    // We check if the second equals 60 "one minute"
+    if (second == 60) {
+      // If so, we add a minute and reset our seconds to 0
+      minute++;
+      second = 0;
+    }
+
+    // If we hit 60 minutes "one hour" we reset the minutes and plus an hour
+    if (minute == 60) {
+      hour++;
+      minute = 0;
+    }
+  }, 1000);
+};
 
 
-//orange
-    circleorange.addEventListener('dragstart', dragStart);
-    circleorange.addEventListener('dragend', dragEnd);
 
 
 
-//Colorblock Listeners
+
+
+
+
+
+//old code
 
 //orange
     orangeBlock.addEventListener('dragover', dragOver);
