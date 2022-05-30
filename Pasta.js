d75cc7f8d608b24d50dc6a968342f8056a27781e
StephGibbons <94647859+StephGibbons@users.noreply.github.com>
2022-05-30 12:24:23 -0230

    'master'

--- Pasta.js
+++ Pasta.js
@@ -1,13 +1,5 @@
 var score = 0;
 
-var min;
-var sec;
-var ms;
-var count;
-var malt;
-var salt;
-var msalt;
-
 const redPepper = document.getElementById('redPepper');
 const bacon = document.getElementById('bacon');
 const eggs = document.getElementById('eggs');
@@ -18,7 +10,7 @@ const tomato = document.getElementById('tomato');
 const butter = document.getElementById('butter');
 const parm = document.getElementById('parm');
 const heavyCream = document.getElementById('heavyCream');
-const basil = document.getElementById('basil');
+const parsley = document.getElementById('parsley');
 const honey = document.getElementById('honey');
 const spaghetti = document.getElementById('spaghetti');
 const cucumber = document.getElementById('cucumber');
@@ -28,61 +20,6 @@ const rotini = document.getElementById('rotini');
 const shoppingCart = document.getElementById('shoppingCart');
 
 
-var stopwatch = {
-  garlict: function(){
-    if(document.getElementById("garlict").firstChild.nodeValue != "garlict"){
-      document.getElementById("garlict").firstChild.nodeValue = "garlict";
-    }
-    ms = 0;
-    sec = 0;
-    min = 0;
-    count = setInterval(function(){
-      if(ms == 100){
-        ms = 0;
-        if(sec == 60){
-          sec = 0;
-          min++;
-        }
-        else{
-          sec++;
-        }
-      }
-      else{
-        ms++;
-      }
-      
-      malt = stopwatch.pad(min);
-      salt = stopwatch.pad(sec);
-      msalt = stopwatch.pad(ms);
-      
-      stopwatch.update(malt + ":" + salt + ":" + msalt);
-    }, 10);
-    },
-  stop: function(){
-    clearInterval(count);
- document.getElementById("garlict").firstChild.nodeValue = "Regarlict";
-  },
-  
-  update: function(txt){
-     var temp = document.getElementById("timer");
-  temp.firstChild.nodeValue = txt;
-  },
-  
-  pad: function(time){
-    var temp;
-    if(time < 10){
-      temp = "0" + time;
-    }
-    else{
-      temp = time;
-    }
-    return temp;
-  }
-}
-
-
-
-//old code
 
 //redPepper
 redPepper.addEventListener('dragstart', dragStart);
@@ -91,7 +28,7 @@ redPepper.addEventListener('dragend', dragEnd);
 bacon.addEventListener('dragstart', dragStart);
 bacon.addEventListener('dragend', dragEnd);
 //eggs
-eggs.addEventListener('dragStart', dragStart);
+eggs.addEventListener('dragstart', dragStart);
 eggs.addEventListener('dragend', dragEnd);
 //greenPepper
 greenPepper.addEventListener('dragstart', dragStart);
@@ -111,9 +48,9 @@ parm.addEventListener('dragend', dragEnd);
 //heavyCream
 heavyCream.addEventListener('dragstart', dragStart);
 heavyCream.addEventListener('dragend', dragEnd);
-//basil
-basil.addEventListener('dragstart', dragStart);
-basil.addEventListener('dragend', dragEnd);
+//parsley
+parsley.addEventListener('dragstart', dragStart);
+parsley.addEventListener('dragend', dragEnd);
 //honey
 honey.addEventListener('dragstart', dragStart);
 honey.addEventListener('dragend', dragEnd);
@@ -130,11 +67,11 @@ oil.addEventListener('dragend', dragEnd);
 rotini.addEventListener('dragstart', dragStart);
 rotini.addEventListener('dragend', dragEnd);
 
-//orange
+//shoppingcart
 shoppingCart.addEventListener('dragover', dragOver);
 shoppingCart.addEventListener('dragenter', dragEnter);
-shoppingCart.addEventListener('dragleave', dragLeaveOrange); 
-shoppingCart.addEventListener('drop', dragDropOrange);
+shoppingCart.addEventListener('dragleave', dragLeave); 
+shoppingCart.addEventListener('drop', dragDrop);
 
 
 
@@ -147,7 +84,7 @@ function dragStart(e){
 }
 
 function dragEnd() {
-    this.className = 'food';
+    
 }
 
 function dragOver(e) {
@@ -160,15 +97,15 @@ function dragEnter(e) {
 
 }
 
-//orange
-function dragLeaveOrange() {
+//dropfunctions
+function dragLeave() {
     this.className = 'shoppingCart';
 }
 
-function dragDropOrange(e) {
+function dragDrop(e) {
     if (mouseOver = "shoppingCart") {
         var elementId = e.dataTransfer.getData("id");
-        if (elementId === "circleorange") {
+        if (elementId === "") {
             this.className = 'ColorOrange';
             this.append(circleorange);
             score++;
@@ -182,7 +119,4 @@ function dragDropOrange(e) {
         }
     }
 }
-
-
-
 
