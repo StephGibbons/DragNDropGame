64ac7b514a8183c368be0ac16d80edcbaf950097
StephGibbons <94647859+StephGibbons@users.noreply.github.com>
2022-06-28 10:04:32 -0230

    'master'

--- Surprise.js
+++ Surprise.js
@@ -1,19 +1,19 @@
 const redPepper = document.getElementById('redPepper');
 const bacon = document.getElementById('bacon');
 const eggs = document.getElementById('eggs');
-const greenPepper = document.getElementById('greenPepper');
+const wsauce = document.getElementById('wsauce');
 const blackPepper = document.getElementById('blackPepper');
 const garlic = document.getElementById('garlic');
+const avocado = document.getElementById('avocado');
+const turkey = document.getElementById('turkey');
 const tomato = document.getElementById('tomato');
-const butter = document.getElementById('butter');
-const parm = document.getElementById('parm');
 const heavyCream = document.getElementById('heavyCream');
 const parsley = document.getElementById('parsley');
 const honey = document.getElementById('honey');
-const spaghetti = document.getElementById('spaghetti');
+const mushroom = document.getElementById('mushroom');
 const cucumber = document.getElementById('cucumber');
 const oil = document.getElementById('oil');
-const rotini = document.getElementById('rotini');
+const buns = document.getElementById('buns');
 
 const shoppingCart = document.getElementById('shoppingCart');
 
@@ -30,21 +30,21 @@ bacon.addEventListener('dragend', dragEnd);
 //eggs
 eggs.addEventListener('dragstart', dragStart);
 eggs.addEventListener('dragend', dragEnd);
-//greenPepper
-greenPepper.addEventListener('dragstart', dragStart);
-greenPepper.addEventListener('dragend', dragEnd);
+//wsauce
+wsauce.addEventListener('dragstart', dragStart);
+wsauce.addEventListener('dragend', dragEnd);
 //blackPepper
 blackPepper.addEventListener('dragstart', dragStart);
 blackPepper.addEventListener('dragend', dragEnd);
+//avocado
+avocado.addEventListener('dragstart', dragStart);
+avocado.addEventListener('dragend', dragEnd);
+//turkey
+turkey.addEventListener('dragstart', dragStart);
+turkey.addEventListener('dragend', dragEnd);
 //tomato
 tomato.addEventListener('dragstart', dragStart);
 tomato.addEventListener('dragend', dragEnd);
-//butter
-butter.addEventListener('dragstart', dragStart);
-butter.addEventListener('dragend', dragEnd);
-//parm
-parm.addEventListener('dragstart', dragStart);
-parm.addEventListener('dragend', dragEnd);
 //heavyCream
 heavyCream.addEventListener('dragstart', dragStart);
 heavyCream.addEventListener('dragend', dragEnd);
@@ -54,18 +54,18 @@ parsley.addEventListener('dragend', dragEnd);
 //honey
 honey.addEventListener('dragstart', dragStart);
 honey.addEventListener('dragend', dragEnd);
-//spaghetti
-spaghetti.addEventListener('dragstart', dragStart);
-spaghetti.addEventListener('dragend', dragEnd);
+//mushroom
+mushroom.addEventListener('dragstart', dragStart);
+mushroom.addEventListener('dragend', dragEnd);
 //cucumber
 cucumber.addEventListener('dragstart', dragStart);
 cucumber.addEventListener('dragend', dragEnd);
 //oil
 oil.addEventListener('dragstart', dragStart);
 oil.addEventListener('dragend', dragEnd);
-//rotini
-rotini.addEventListener('dragstart', dragStart);
-rotini.addEventListener('dragend', dragEnd);
+//buns
+buns.addEventListener('dragstart', dragStart);
+buns.addEventListener('dragend', dragEnd);
 //garlic
 garlic.addEventListener('dragstart', dragStart);
 garlic.addEventListener('dragend', dragEnd);
@@ -135,9 +135,9 @@ function dragDrop(e){
               }
           }
 
-          if(elementId === "parm"){
+          if(elementId === "tomato"){
             this.elementId = 'shoppingCart';
-            this.append(parm);
+            this.append(tomato);
             this.classList.remove('glow') 
             score++; 
                   
@@ -148,9 +148,9 @@ function dragDrop(e){
                   }
             }
 
-            if(elementId === "spaghetti"){
+            if(elementId === "mushroom"){
               this.elementId = 'shoppingCart';
-              this.append(spaghetti);
+              this.append(mushroom);
               this.classList.remove('glow') 
               score++; 
                     
